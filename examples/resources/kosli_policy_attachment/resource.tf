83eba2351e7722a20f45ae14bcfb042100eaaf85
Marko Bevc <marko@kosli.com>
2026-03-19 13:33:43 +0000

    chore(kosli_policy_attachment): simplify TF examples

--- examples/resources/kosli_policy_attachment/resource.tf
+++ examples/resources/kosli_policy_attachment/resource.tf
@@ -8,14 +8,7 @@ terraform {
 
 # Attach a policy to an environment.
 # Both the policy and environment must exist before creating the attachment.
-resource "kosli_policy_attachment" "resources_example" {
-  environment_name = kosli_environment.example.name
-  policy_name      = kosli_policy.example.name
-}
-
-# Attach a policy to an environment.
-# Both the policy and environment must exist before creating the attachment.
-resource "kosli_policy_attachment" "simple_example" {
+resource "kosli_policy_attachment" "example" {
   environment_name = "my-environment"
   policy_name      = "my-policy"
 }
